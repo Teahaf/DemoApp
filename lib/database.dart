7c094865728c9532daf8dfb8baac1828475135b8
Teodora <teodorahafner2304@gmail.com>
2020-11-01 18:09:38 +0100

    initial commit

--- lib/database.dart
+++ lib/database.dart
@@ -1,11 +1,11 @@
 import 'dart:io';
 
+import 'package:flutter_abc/main.dart';
 import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
 
 class DatabaseHelper {
-
   static final _databaseName = "MyDatabase.db";
   static final _databaseVersion = 1;
 
@@ -17,12 +17,25 @@ class DatabaseHelper {
   static final columnFoodCarbohydrates = 'carbohydrates';
   static final columnFoodFat = 'fat';
 
+  static final table_logs = 'logs';
+  static final columnLogId = '_id';
+  static final columnFoodIdForLog = 'food_id';
+  static final columnLogDate = 'date';
+
+  static final table_changed_activity = 'changedActivity';
+  static final column_changed_activity_Id = '_id';
+  static final column_startActivity = 'start_activity';
+  static final column_endActivity = 'end_activity';
+  static final column_timeInMilis = 'timeInMillis';
+
   // make this a singleton class
   DatabaseHelper._privateConstructor();
+
   static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
 
   // only have a single app-wide reference to the database
   static Database _database;
+
   Future<Database> get database async {
     if (_database != null) return _database;
     // lazily instantiate the db the first time it is accessed
@@ -35,26 +48,84 @@ class DatabaseHelper {
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
     String path = join(documentsDirectory.path, _databaseName);
     return await openDatabase(path,
-        version: _databaseVersion,
-        onCreate: _onCreate);
+        version: _databaseVersion, onCreate: _onCreate);
   }
 
   // SQL code to create the database table
   Future _onCreate(Database db, int version) async {
     await db.execute('''
           CREATE TABLE $table_foods (
-            $columnFoodId INTEGER PRIMARY KEY,
+            $columnFoodId integer primary key autoincrement,
             $columnFoodName TEXT NOT NULL,
             $columnFoodEnergy REAL NOT NULL,
             $columnFoodProtein REAL NOT NULL,
             $columnFoodCarbohydrates REAL NOT NULL,
             $columnFoodFat REAL NOT NULL
-          )''');
+          )
+         
+          ''');
+    await db.execute('''
+         CREATE TABLE $table_logs (
+         $columnLogId integer primary key autoincrement,
+         $columnFoodIdForLog TEXT NOT NULL,
+         $columnLogDate TEXT NOT NULL
+               )   ''');
+    await db.execute('''
+         CREATE TABLE $table_changed_activity (
+         $column_changed_activity_Id integer primary key autoincrement,
+         $column_startActivity TEXT NOT NULL,
+         $column_endActivity TEXT NOT NULL,
+         $column_timeInMilis INT NOT NULL
+               )   ''');
   }
 
-  Future<int> insert(Map<String, dynamic> row) async {
+  Future<int> insertChangedActivity(Map<String, dynamic> row) async {
+    Database db = await instance.database;
+    return await db.insert(table_changed_activity, row);
+  }
+
+  Future<List<ChangedActivity>> getActivitiesThatStartWith(String startActivity) async {
+    Database db = await instance.database;
+    var rows = await db.rawQuery('SELECT * FROM $table_changed_activity WHERE $column_startActivity=?', [startActivity]);
+
+    var array = List<ChangedActivity>();
+    for (int i = 0; i < rows.length; i++) {
+      Map<String, dynamic> row = rows[i];
+      ChangedActivity obj  = ChangedActivity(row[column_changed_activity_Id], row[column_startActivity], row[column_endActivity],
+          row[column_timeInMilis]);
+      array.add(obj);
+    }
+    return array;
+  }
+
+  Future<List<ChangedActivity>> getActivities() async {
+    Database db = await instance.database;
+    var rows = await db.rawQuery('SELECT * FROM $table_changed_activity');
+
+    var array = List<ChangedActivity>();
+    for (int i = 0; i < rows.length; i++) {
+      Map<String, dynamic> row = rows[i];
+      ChangedActivity obj  = ChangedActivity(row[column_changed_activity_Id], row[column_startActivity], row[column_endActivity],
+          row[column_timeInMilis]);
+      array.add(obj);
+    }
+    return array;
+  }
+
+  Future<int> insertFood(Map<String, dynamic> row) async {
     Database db = await instance.database;
     return await db.insert(table_foods, row);
+  }
+
+  Future<int> insertLog(Map<String, dynamic> row) async {
+    Database db = await instance.database;
+    return await db.insert(table_logs, row);
+  }
+
+  Future<List<Map<String, dynamic>>> getLogsByDate(String date) async {
+    Database db = await instance.database;
+    return await db
+        .rawQuery('SELECT * FROM $table_logs WHERE $columnLogDate = $date');
   }
 
   Future<List<Map<String, dynamic>>> queryAllRows() async {
@@ -64,17 +135,20 @@ class DatabaseHelper {
 
   Future<int> queryRowCount() async {
     Database db = await instance.database;
-    return Sqflite.firstIntValue(await db.rawQuery('SELECT COUNT(*) FROM $table_foods'));
+    return Sqflite.firstIntValue(
+        await db.rawQuery('SELECT COUNT(*) FROM $table_foods'));
   }
 
   Future<int> update(Map<String, dynamic> row) async {
     Database db = await instance.database;
     int id = row[columnFoodId];
-    return await db.update(table_foods, row, where: '$columnFoodId = ?', whereArgs: [id]);
+    return await db
+        .update(table_foods, row, where: '$columnFoodId = ?', whereArgs: [id]);
   }
 
   Future<int> delete(int id) async {
     Database db = await instance.database;
-    return await db.delete(table_foods, where: '$columnFoodId = ?', whereArgs: [id]);
+    return await db
+        .delete(table_foods, where: '$columnFoodId = ?', whereArgs: [id]);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,27 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:activity_recognition_flutter/activity_recognition_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_abc/ticker.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart' as http;
+import 'package:permission_handler/permission_handler.dart';
 
 import 'database.dart';
 
 // ignore: non_constant_identifier_names
 String API_KEY = "Unjc2Z4luZu0sKFBGflwS7cnxEiU83YygiIU37Ul";
+String oldActivity;
+int activityStarted = 0;
+final dbHelper = DatabaseHelper.instance;
+
+Future<List<Food>> futureAlbum;
+ActivityType _activityState = ActivityType.STILL;
+int caloriesBurned = 0;
+String timePassed;
+Map activities = Map();
+final TimerBloc _timerBloc = TimerBloc(ticker: Ticker());
 
 Future<List<Food>> fetchAlbum() async {
   final response = await http.get(
@@ -32,17 +46,17 @@ List<Food> fromJson(Map<String, dynamic> json) {
       switch (elementObject['nutrientName']) {
         case "Energy":
           if (elementObject['unitName'] == "KCAL")
-            energy = elementObject['value'];
+            energy = double.parse(elementObject['value']);
           break;
         case "Protein":
-          protein = elementObject['value'];
+          protein = double.parse(elementObject['value']);
           break;
         case "Carbohydrate, by difference":
-          carbohydrates = elementObject['value'];
+          carbohydrates = double.parse(elementObject['value']);
           break;
-        case "Total lipid (fat)":
-          fat = elementObject['value'];
-          break;
+        // case "Total lipid (fat)":
+        //   fat = double.parse(elementObject['value']);
+        //   break;
         default:
           break;
       }
@@ -81,7 +95,17 @@ class Food {
   Widget buildSubtitle(BuildContext context) {}
 }
 
-void main() => runApp(MyApp());
+class ChangedActivity {
+  final int id;
+  final String startActivity;
+  final String endActivity;
+  final int elapsedTimeInMillis;
+
+  ChangedActivity(
+      this.id, this.startActivity, this.endActivity, this.elapsedTimeInMillis);
+}
+
+
 
 class MyApp extends StatefulWidget {
   MyApp({Key key}) : super(key: key);
@@ -91,99 +115,308 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Future<List<Food>> futureAlbum;
-  final dbHelper = DatabaseHelper.instance;
-
   @override
   void initState() {
     super.initState();
     futureAlbum = fetchAlbum();
+    _initStreamActivity();
+  }
+
+  @override
+  void dispose() {
+    _timerBloc.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     final title = 'Mixed List';
-    // Future<List<Food>> items = futureAlbum;
     return MaterialApp(
       title: title,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: FutureBuilder<List<Food>>(
-          future: futureAlbum,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    GestureDetector(
-                        onTap: () {
-                          _insert(snapshot.data[0]);
-                        },
-                        child: new Container(
-                          width: 500.0,
-                          padding:
-                              new EdgeInsets.fromLTRB(20.0, 40.0, 20.0, 40.0),
-                          color: Colors.green,
-                          child: new Column(children: [
-                            new Text((snapshot.data[0].name)),
-                          ]),
-                        )),
-                    Text(snapshot.data[1].name),
-                    Text(snapshot.data[2].name),
-                    Text(snapshot.data[3].name),
-                    Text(snapshot.data[4].name),
-                    Text(snapshot.data[5].name),
-                  ]);
-            } else if (snapshot.hasError) {
-              return Text("${snapshot.error}");
-            }
-            // By default, show a loading spinner.
-            return CircularProgressIndicator();
-          },
-        ),
+      // home: Scaffold(
+      //     appBar: AppBar(
+      //       title: Text(title),
+      //     ),
+      //     body: Column(
+      //         mainAxisAlignment: MainAxisAlignment.center,
+      //         children: <Widget>[
+      //           Text(_activityState.toString()),
+      //           Text(timePassed.toString()),
+      //           Center(
+      //             child: BlocBuilder(
+      //               bloc: _timerBloc,
+      //               builder: (context, state) {
+      //                 final String minutesStr = ((state.duration / 60) % 60)
+      //                     .floor()
+      //                     .toString()
+      //                     .padLeft(2, '0');
+      //                 final String secondsStr =
+      //                 (state.duration % 60).floor().toString().padLeft(2, '0');
+      //                 return Text(
+      //                   '$minutesStr:$secondsStr',
+      //                   style: Timer.timerTextStyle,
+      //                 );
+      //               },
+      //
+      //             )
+      //           ),
+      //         ])
+      // ),
+      home: BlocProvider(
+        bloc: _timerBloc,
+        child: Timer(),
       ),
     );
   }
 
-  void _insert(Food data) async {
-    // row to insert
+  Stream<Activity> activityStream;
+
+  void _initStreamActivity() async {
+    if (await Permission.activityRecognition.request().isGranted) {
+      activityStream = ActivityRecognition.activityUpdates();
+      activityStream.listen(onData);
+    } else if (Permission.activityRecognition.isGranted != null) {
+      activityStream = ActivityRecognition.activityUpdates();
+      activityStream.listen(onData);
+    }
+  }
+
+  void _insertChangedActivity(ChangedActivity data) async {
     Map<String, dynamic> row = {
-      DatabaseHelper.columnFoodName: data.name,
-      DatabaseHelper.columnFoodEnergy: data.energy,
-      DatabaseHelper.columnFoodProtein: data.protein,
-      DatabaseHelper.columnFoodCarbohydrates: data.carbohydrates,
-      DatabaseHelper.columnFoodFat: data.fat
+      DatabaseHelper.column_startActivity: data.startActivity,
+      DatabaseHelper.column_endActivity: data.endActivity,
+      DatabaseHelper.column_timeInMilis: data.elapsedTimeInMillis
     };
-    final id = await dbHelper.insert(row);
+    final id = await dbHelper.insertChangedActivity(row);
     print('inserted row id: $id');
   }
 
-  void _query() async {
-    final allRows = await dbHelper.queryAllRows();
-    print('query all rows:');
-    allRows.forEach((row) => print(row));
+  Future<void> onData(Activity activity) async {
+    ActivityType type = activity.type;
+    int confidence = activity.confidence;
+    _saveChangedActivityInDatabase(type);
+    // await _calculateCaloriesBurned();
   }
 
-  void _update() async {
-    // row to update
-    Map<String, dynamic> row = {
-      DatabaseHelper.columnFoodId: 1,
-      DatabaseHelper.columnFoodName: 'Mary',
-      DatabaseHelper.columnFoodFat: 47,
-      DatabaseHelper.columnFoodCarbohydrates: 5,
-      DatabaseHelper.columnFoodProtein: 41,
-      DatabaseHelper.columnFoodEnergy: 12
-    };
-    final rowsAffected = await dbHelper.update(row);
-    print('updated $rowsAffected row(s)');
+  void _saveChangedActivityInDatabase(ActivityType type) {
+    switch (type) {
+      case ActivityType.STILL:
+        if (!activities.containsKey(ActivityType.STILL)) {
+          activities[ActivityType.STILL] =
+              DateTime.now().millisecondsSinceEpoch;
+        }
+        break;
+      case ActivityType.IN_VEHICLE:
+        if (!activities.containsKey(ActivityType.IN_VEHICLE)) {
+          activities[ActivityType.IN_VEHICLE] =
+              DateTime.now().millisecondsSinceEpoch;
+        }
+        break;
+      case ActivityType.ON_BICYCLE:
+        activities[ActivityType.ON_BICYCLE] =
+            DateTime.now().millisecondsSinceEpoch;
+        break;
+      case ActivityType.ON_FOOT:
+        if (!activities.containsKey(ActivityType.ON_FOOT)) {
+          activities[ActivityType.ON_FOOT] =
+              DateTime.now().millisecondsSinceEpoch;
+        }
+        break;
+      case ActivityType.RUNNING:
+        if (!activities.containsKey(ActivityType.RUNNING)) {
+          activities[ActivityType.RUNNING] =
+              DateTime.now().millisecondsSinceEpoch;
+        }
+        break;
+      case ActivityType.TILTING:
+        if (!activities.containsKey(ActivityType.TILTING)) {
+          activities[ActivityType.TILTING] =
+              DateTime.now().millisecondsSinceEpoch;
+        }
+        break;
+      case ActivityType.UNKNOWN:
+        if (!activities.containsKey(ActivityType.UNKNOWN)) {
+          activities[ActivityType.UNKNOWN] =
+              DateTime.now().millisecondsSinceEpoch;
+        }
+        break;
+      case ActivityType.WALKING:
+        if (!activities.containsKey(ActivityType.WALKING)) {
+          activities[ActivityType.WALKING] =
+              DateTime.now().millisecondsSinceEpoch;
+        }
+        break;
+      case ActivityType.INVALID:
+        if (!activities.containsKey(ActivityType.INVALID)) {
+          activities[ActivityType.INVALID] =
+              DateTime.now().millisecondsSinceEpoch;
+        }
+        break;
+      // }
+    }
+    _insertChangedActivity(new ChangedActivity(
+        0,
+        type.toString(),
+        _activityState.toString(),
+        DateTime.now().millisecond - activityStarted));
+    activityStarted = DateTime.now().millisecond;
+
+    dbHelper
+        .getActivitiesThatStartWith(ActivityType.ON_FOOT.toString())
+        .then((value) => {
+              if ((value == null) || (value != null && _activityState != type))
+                {
+                  _insertChangedActivity(new ChangedActivity(
+                      0,
+                      type.toString(),
+                      _activityState.toString(),
+                      DateTime.now().millisecond - activityStarted)),
+                  activityStarted = DateTime.now().millisecond,
+                }
+            });
+
+    _activityState = type;
+  }
+}
+
+class Timer extends StatelessWidget {
+  static const TextStyle timerTextStyle = TextStyle(
+    fontSize: 60,
+    fontWeight: FontWeight.bold,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final TimerBloc _timerBloc = BlocProvider.of<TimerBloc>(context);
+    return Scaffold(
+      appBar: AppBar(title: Text('Flutter Timer')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 100.0),
+            child: Center(
+              child: BlocBuilder(
+                bloc: _timerBloc,
+                builder: (context, state) {
+                  final String minutesStr = ((state.duration / 60) % 60)
+                      .floor()
+                      .toString()
+                      .padLeft(2, '0');
+                  final String secondsStr =
+                      (state.duration % 60).floor().toString().padLeft(2, '0');
+                  return Text(
+                    '$minutesStr:$secondsStr',
+                    style: Timer.timerTextStyle,
+                  );
+                },
+              ),
+            ),
+          ),
+          BlocBuilder(
+            condition: (previousState, currentState) =>
+                currentState.runtimeType != previousState.runtimeType,
+            bloc: _timerBloc,
+            builder: (context, state) => Actions(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Actions extends StatefulWidget {
+  @override
+  _ActionState createState() => _ActionState();
+
+}
+
+class _ActionState extends State<Actions>{
+  String s = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: _mapStateToActionButtons(
+        timerBloc: BlocProvider.of<TimerBloc>(context),
+      ),
+    );
+  }
+  List<Widget> _mapStateToActionButtons({
+    TimerBloc timerBloc,
+  }) {
+    final TimerState state = timerBloc.currentState;
+    if (state is Ready) {
+      calculateCalories();
+      return [
+        FloatingActionButton(
+          child: Icon(Icons.play_arrow),
+          onPressed: () => timerBloc.dispatch(Start(duration: state.duration)),
+        ),
+      ];
+    }
+    if (state is Running) {
+      return [
+        FloatingActionButton(
+          child: Icon(Icons.pause),
+          onPressed: () => timerBloc.dispatch(Pause()),
+        ),
+        FloatingActionButton(
+          child: Icon(Icons.replay),
+          onPressed: () => timerBloc.dispatch(Reset()),
+        ),
+      ];
+    }
+    if (state is Paused) {
+
+      return [
+        FloatingActionButton(
+          child: Icon(Icons.play_arrow),
+          onPressed: () => timerBloc.dispatch(Resume()),
+        ),
+        FloatingActionButton(
+          child: Icon(Icons.replay),
+          onPressed: () => timerBloc.dispatch(Reset()),
+        ),
+      ];
+    }
+    if (state is Finished) {
+      return [
+        Column(
+          children: [
+            FloatingActionButton(
+              child: Icon(Icons.replay),
+              onPressed: () => timerBloc.dispatch(Reset()),
+            ),
+            Text(s)
+          ],
+        ),
+      ];
+    }
+    return [];
   }
 
-  void _delete() async {
-    // Assuming that the number of rows is the id for the last row.
-    final id = await dbHelper.queryRowCount();
-    final rowsDeleted = await dbHelper.delete(id);
-    print('deleted $rowsDeleted row(s): row $id');
+  void calculateCalories() {
+    int timeElapsed = 0;
+    double timeElapsedInHours, BMR;
+    int weight, height, age;
+
+    dbHelper
+        .getActivitiesThatStartWith(ActivityType.STILL.toString())
+        .then((value) => {
+      value.forEach((element) {
+        timeElapsed += element.elapsedTimeInMillis;
+      }),
+
+      timeElapsedInHours = (timeElapsed/ 1000) ,
+      weight = 64,
+      height = 174,
+      age = 23,
+      BMR = 66.47 + (13.75 * weight) + (5.003 * height) - (6.755 * age),
+      s = (BMR * 4.3 / 24 * timeElapsedInHours).toString(),
+      setState(() {})
+    });
   }
 }
